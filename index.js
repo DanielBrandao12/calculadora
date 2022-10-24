ae9426fdb12d53a8a7b737c2773bdf9dbae2e36d
DanielBrandao12 <drb12da@gmail.com>
2022-10-24 14:58:25 -0300

    criando eventos

--- index.js
+++ index.js
@@ -2,29 +2,62 @@
 const visor = document.querySelector('.visor')
 const buttons = document.querySelectorAll('.button')
 
-
-
+let numeros = []
 
 buttons.forEach(buttonsId => {
-    buttonsId.addEventListener('click', e=>{
-        let valor = buttonsId.innerText
-        if(visor.innerText==0){
-            if(valor == '.'){
-                visor.innerText + valor
-            }
-            else{
+    buttonsId.addEventListener('click', e => {
+        let valorClicado = buttonsId.innerText
+            console.log(valorClicado)
+            numeros.push(valorClicado)
+            visor.innerText = numeros
+    })
+})
 
-                visor.innerText=""
-            }
+
+    /*  if(!isNaN(valorClicado)){
+
+            visor.innerText = valorClicado
+            num2= visor.innerText 
+        }
+
+     
+        let opcao=valorClicado
+       switch(opcao){
+
+            case '+': 
+            num1= visor.innerText 
+             opcao ='+'
+            break
+            case '-': 
+            num1= visor.innerText 
+            opcao ='-'
+            break
+            case '*': 
+            num1= visor.innerText 
+            opcao ='*'
+            break
+            case '/': 
+            num1= visor.innerText 
+            opcao ='/'
+            break
+        
+       }
+       if(valorClicado == '='){
+        console.log(opcao)
+       
+         console.log(  soma(parseInt(num1), parseInt(num2)))
+        
           
-        }
-        if(visor.innerText !== 0 ){
-
-            visor.innerText +=valor
-            
-    console.log( visor.innerText)
-        }
-       
-       
-    } )
-} )
+         function soma(n1,n2){
+             return n1+n2
+         }
+         function sub(n1,n2){
+             return n1+n2
+         }
+         function mmulti(n1,n2){
+             return n1+n2
+         }
+         function divi(n1,n2){
+             return n1+n2
+         }
+        }*/

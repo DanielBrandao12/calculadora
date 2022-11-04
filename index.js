294e0259aa2496acaf3406cd8953880bb0f273b1
DanielBrandao12 <drb12da@gmail.com>
2022-11-04 15:48:08 -0300

    verificação se ponto foi clicado e atualização das operações

--- index.js
+++ index.js
@@ -2,123 +2,100 @@
 const visor = document.querySelector('.visor')
 const buttons = document.querySelectorAll('.button')
 
-let numeros 
+let numeros
 let operacao
-let tela
+let ponto = '.'
+let sinais = ['+', '-', '*', '/']
+let operacoes = []
 
 buttons.forEach(buttonsId => {
+    
     buttonsId.addEventListener('click', e => {
+
         let valorClicado = buttonsId.innerText
 
 
-           console.log(visor.innerText)
-       
-         
-            if(!isNaN(valorClicado)){
-                if(visor.innerText == 0){
-                    visor.innerText = ''
-                }
+        if (!isNaN(valorClicado)) {
+
+            if (visor.innerText == '0') {
+
+                visor.innerText = ''
+
+            }
+            if (sinais.includes(operacoes[0])) {
+                visor.innerText = ''
+                operacoes = []
+            }
+            if (visor.innerText.length < 10) {
+
                 visor.innerText += valorClicado
-            }else if(valorClicado =='.') {
-         
-                    visor.innerText += valorClicado
+            }
+
+        }else if (!visor.innerText.includes(ponto)) {
+
+             if (valorClicado == '.') {
                 
+                console.log(visor.innerText += valorClicado)
+
             }
 
-            console.log(parseFloat(visor.innerText))
-            if(valorClicado =='+'){
-                numeros= visor.innerText
-                operacao = '+'
-                visor.innerText =''
-               // console.log(numeros)
-            }
-            if(valorClicado =='-'){
-                numeros= visor.innerText
-                operacao = '-'
-                visor.innerText =''
-               // console.log(numeros)
-            }
-            if(valorClicado =='*'){
-                numeros= visor.innerText
-                operacao = '*'
-                visor.innerText =''
-               // console.log(numeros)
-            }
-            if(valorClicado =='/'){
-                numeros= visor.innerText
-                operacao = '/'
-                visor.innerText =''
-               // console.log(numeros)
-            }
-        
-            if(valorClicado =='='){
-               if(operacao =='+'){
-                  
-                return   console.log(visor.innerText= parseFloat(numeros)+parseFloat(visor.innerText))
-
-               }else if(operacao =='-'){
-                  
-                return   console.log(visor.innerText= parseFloat(numeros)-parseFloat(visor.innerText))
-
-               }else if(operacao =='*'){
-                  
-                return   console.log(visor.innerText= parseFloat(numeros)*parseFloat(visor.innerText))
-
-               }else if(operacao =='/'){
-                  
-                return   console.log(visor.innerText= parseFloat(numeros)/parseFloat(visor.innerText))
-
-               }
-               
-            }
-    })
-})
-
-
-    /*  if(!isNaN(valorClicado)){
-
-            visor.innerText = valorClicado
-            num2= visor.innerText 
         }
 
-     
-        let opcao=valorClicado
-       switch(opcao){
 
-            case '+': 
-            num1= visor.innerText 
-             opcao ='+'
-            break
-            case '-': 
-            num1= visor.innerText 
-            opcao ='-'
-            break
-            case '*': 
-            num1= visor.innerText 
-            opcao ='*'
-            break
-            case '/': 
-            num1= visor.innerText 
-            opcao ='/'
-            break
-        
-       }
-       if(valorClicado == '='){
-        console.log(opcao)
-       
-         console.log(  soma(parseInt(num1), parseInt(num2)))
-        
-          
-         function soma(n1,n2){
-             return n1+n2
-         }
-         function sub(n1,n2){
-             return n1+n2
-         }
-         function mmulti(n1,n2){
-             return n1+n2
-         }
-         function divi(n1,n2){
-             return n1+n2
-         }
-        }*/
+
+        if (valorClicado == '+') {
+            numeros = visor.innerText
+            operacao = '+'
+            operacoes.push(operacao)
+
+        }
+        if (valorClicado == '-') {
+            numeros = visor.innerText
+            operacao = '-'
+            operacoes.push(operacao)
+        }
+        if (valorClicado == '*') {
+            numeros = visor.innerText
+            operacao = '*'
+            operacoes.push(operacao)
+        }
+        if (valorClicado == '/') {
+            numeros = visor.innerText
+            operacao = '/'
+            operacoes.push(operacao)
+        }
+        if (valorClicado == 'C') {
+            visor.innerText = 0
+        }
+
+        if (valorClicado == '=') {
+            if (operacao == '+') {
+                if (visor.innerText < '9999999999') {
+                    return visor.innerText = parseFloat(numeros) + parseFloat(visor.innerText)
+                }
+
+
+            } else if (operacao == '-') {
+
+                return visor.innerText = parseFloat(numeros) - parseFloat(visor.innerText)
+
+
+            } else if (operacao == '*') {
+
+                if (visor.innerText < '9999999999') {
+                    return visor.innerText = parseFloat(numeros) * parseFloat(visor.innerText)
+                }
+
+
+            } else if (operacao == '/') {
+
+
+                return visor.innerText = parseFloat(numeros) / parseFloat(visor.innerText)
+
+
+
+            }
+
+        }
+    })
+})
